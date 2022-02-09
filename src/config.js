6cdd7e794645dddef43c3af0e599d257c116099a
Mirza <muhammedmirzakilic@gmail.com>
2022-02-09 23:04:35 +0300

    serverless offline env bug fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,15 @@
+//serverless offline env vars issue workaround
+const host =
+  process.env.REDIS_HOST === "[object Object]"
+    ? "localhost"
+    : process.env.REDIS_HOST;
+const port =
+  process.env.REDIS_PORT === "[object Object]" ? 6379 : process.env.REDIS_PORT;
+
 module.exports = {
   redis: {
-    host: process.env.REDIS_HOST || "localhost",
-    port: process.env.REDIS_PORT || 6379,
+    host: host,
+    port: port,
     password: process.env.REDIS_PASSWORD || "",
     matchDB: process.env.REDIS_MATCH_DB || 0,
     leaderboardDB: process.env.REDIS_LEADERBOARD_DB || 1,
